7d33c7fda6ec06fecfb742a683819546ecbd989d
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-11-01 20:08:49 +0530

    1

---
+++ WebAPIDemo/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIDemo.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAPIDemoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAPIDemoContext") ?? throw new InvalidOperationException("Connection string 'WebAPIDemoContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
